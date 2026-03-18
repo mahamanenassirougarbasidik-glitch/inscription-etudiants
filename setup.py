184d674fc838097a54974d2586fe143c9f3fafeb
Aboubacar Sidik <mahamanenassirougarbasidik@gmail.com>
2026-03-18 02:46:11 +0100

    Ajout de setup.py et correction du workflow

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="inscription_etudiants",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Les dépendances sont déjà dans requirements.txt
+        # On peut les laisser vides ou les lister, mais on utilisera requirements.txt
+    ],
+)
